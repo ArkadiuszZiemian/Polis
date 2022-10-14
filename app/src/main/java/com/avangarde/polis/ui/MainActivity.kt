ff86e0a4a6368dee017aef16f4fb4e7434e4346a
DictumFactum <lmc195@protonmail.com>
2022-10-14 02:59:30 +0200

    feat: build login screen layout

--- app/src/main/java/com/avangarde/polis/ui/MainActivity.kt
+++ app/src/main/java/com/avangarde/polis/ui/MainActivity.kt
@@ -16,9 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        this.supportActionBar?.hide()
         setContentView(R.layout.activity_main)
-
     }
-
-
 }
